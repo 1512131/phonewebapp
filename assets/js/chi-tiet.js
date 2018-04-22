4db6befabd992cf8ad833b6e2ea4631bee5b6090
1512131 <1512131@student.hcmus.edu.vn>
2018-04-22 23:09:45 +0700

    1. Tạo file html chi tiết cho một số sản phẩm.
    2. Tạo file javascript chi-tiet.css dùng chung cho chi tiết sản phẩm.
    3. Sửa file dien-thoai.js: thêm các hàm tìm sản phẩm, xem sản phẩm theo nhà sản xuất.
    4. Sửa file dien-thoai.html: chỉ thay thế các tên lớp, text, ...
    5. Tách file dien-thoai.css ra thêm file menu.css: cấu hình menu bar.
    6. Thêm một số ảnh.

---
+++ assets/js/chi-tiet.js
@@ -0,0 +1,13 @@
+$(document).ready(function()
+{
+	show(1);
+});
+
+function show(n)
+{
+	for (var i = 0; i < $(".large").size(); i++)
+	{
+		$(".large").get(i).style.display = "none";
+	}
+	$(".large").get(n - 1).style.display = "inline-block";
+}
--- assets/js/dien-thoai.js
+++ assets/js/dien-thoai.js
@@ -1,55 +1,65 @@
 $(document).ready(function()
 {
-	for (var i = 16; i < $(".col-lg-3").size(); i++)
-	{
-		$(".col-lg-3").get(i).style.display = "none";
-	}
+	showFrom(0);
+	$("#page1").attr('disabled','disabled');
 });
+
+function hideAll() {
+	for (var i = 0; i < $(".item").size(); i++)
+	{
+		$(".item").get(i).style.display = "none";
+	}
+}
+
+function showFrom(index) {
+	hideAll();
+	for (var i = index; i < $(".item").size() && i < index + 16; i++)
+	{
+		$(".item").get(i).style.display = "inline-block";
+	}
+}
 
 $("#page1").bind("click",function () {
 	$("#page2").removeAttr('disabled');
-	for (var i = 0; i < 16; i++)
-	{
-		$(".col-lg-3").get(i).style.display = "inline-block";
-	}
-    for (var i = 16; i < $(".col-lg-3").size(); i++)
-	{
-		$(".col-lg-3").get(i).style.display = "none";
-	}
+	showFrom(0);
 	$("#page1").attr('disabled','disabled');
 });
 
 $("#page2").bind("click",function () {
-	$("#page1").removeAttr('disabled');
-	for (var i = 0; i < 16; i++)
-	{
-		$(".col-lg-3").get(i).style.display = "none";
+		$("#page1").removeAttr('disabled');
+		showFrom(16);
+		$("#page2").attr('disabled','disabled');
+});
+
+function search() {
+	if ($('#search-box').val().length == 0) {
+		alert("Nhập tên sản phẩm bạn muốn tìm.");
+		return;
 	}
-    for (var i = 16; i < $(".col-lg-3").size(); i++)
+	hideAll();
+	$("#page1").hide();
+	$("#page2").hide();
+	var inputName = $('#search-box').attr("checked", true).val().toLowerCase();
+	var name;
+	for (var i = 0; i < $(".item").size(); i++)
 	{
-		$(".col-lg-3").get(i).style.display = 'inline-block';
-	}
-	$("#page2").attr('disabled','disabled');
-});
-
-$("#phone").bind("click",function () {
-    window.location = 'index.html';
-});
-$("#tablet").bind("click",function () {
-    window.location = 'index.html';
-});
-
-
-
-
-
-$("#sign-up").bind("click",function()
-{	
-		for (var i = 0; i < $(".hidden").size(); i++)
-		{
-			var name = $('span[name=ngay]');
-			var innerHTML = $('span[name=ngay]').get(i).innerHTML;
-			
-			alert(name + "-->" + innerHTML);
+		name = $(".card-title").get(i).innerHTML.toLowerCase();
+		if (name.indexOf(inputName) != -1) {
+			$(".item").get(i).style.display = "inline-block";
 		}
-});
+	}
+}
+
+function showPhone(phoneName)
+{
+	hideAll();
+	$("#page1").hide();
+	$("#page2").hide();
+	for (var i = 0; i < $(".item").size(); i++)
+	{
+		name = $(".nha-san-xuat").get(i).innerHTML;
+		if (name.indexOf(phoneName) != -1) {
+			$(".item").get(i).style.display = "inline-block";
+		}
+	}
+}
