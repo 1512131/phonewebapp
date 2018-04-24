4365c8f984e512b8e0d222ef34272bcce1ce3476
ducnguyenhoai3997 <1512124@student.hcmus.edu.vn>
2018-04-24 14:40:42 +0700

    Tạo file index.htm: hiển thị danh sách các sản phẩm mới nhất, bán chạy nhất và được xem nhiều nhất

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	setVisible('.new_', 1);
+	setVisible('.best_selling_', 1);
+	setVisible('.most_view_', 1);
+	
+});
+
+function setVisible(str, index){
+	for (var i=1; i<=3; i++){
+		var select = str + i;
+		$(select).hide();
+	}
+	var select = str + index;
+		$(select).show();
+}
+
+$('.new_pagination_item').bind('click', function(){
+	setVisible('.new_', $(this).html());
+	$('.new_pagination_item').css('background-color', 'white');
+	$('.new_pagination_item').css('color', 'black');
+	$(this).css('background-color', 'blue');
+	$(this).css('color', 'white');
+});
+
+$('.best_selling_pagination_item').bind('click', function(){
+	setVisible('.best_selling_', $(this).html());
+	$('.best_selling_pagination_item').css('background-color', 'white');
+	$('.best_selling_pagination_item').css('color', 'black');
+	$(this).css('background-color', 'blue');
+	$(this).css('color', 'white');
+});
+
+$('.most_view_pagination_item').bind('click', function(){
+	setVisible('.most_view_', $(this).html());
+	$('.most_view_pagination_item').css('background-color', 'white');
+	$('.most_view_pagination_item').css('color', 'black');
+	$(this).css('background-color', 'blue');
+	$(this).css('color', 'white');
+});
