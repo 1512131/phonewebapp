6cdbbfac1b949ccb93047a821550803373056c3e
1512131 <1512131@student.hcmus.edu.vn>
2018-04-27 19:03:08 +0700

    1. Tạo các trang chi tiết sản phẩm khi đã đăng nhập.
    2. Thêm máy tính bảng vào trang sản phẩm khi đã đăng nhập.

--- assets/js/chi-tiet.js
+++ assets/js/chi-tiet.js
@@ -1,16 +1,6 @@
 $(document).ready(function()
 {
 	show(1);
-	if (location.hash === "#chon-mua") {
-		$("#menu-id").hide();
-		$("#menu-mua-hang-id").show();
-		$(".ct-btn").removeClass('hidden');
-	}
-	else {
-		$("#menu-id").show();
-		$("#menu-mua-hang-id").hide();
-		$(".ct-btn").attr('class','hidden');
-	}
 });
 
 function show(n)
